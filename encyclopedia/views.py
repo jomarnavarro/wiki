3c1ddd5457b311328ee8b4b8dbef55bd3b6e3624
Omar Navarro <jomarnavarro@gmail.com>
2021-06-15 13:52:41 -0500

    Implements the search functionality and fixes the wiki route

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,6 +9,7 @@ def index(request):
     })
 
 def title(request, title):
+    print(title)
     entry = util.get_entry(title)
     if not entry:
         return render(request, "encyclopedia/error.html", {
@@ -19,3 +20,30 @@ def title(request, title):
         "entry": entry
     })
 
+def search(request):
+    # gets the q value from field
+    title = request.GET['q']
+    # tries to get entry for q.  if so, it loads that page.
+    entry = util.get_entry(title)
+    if entry:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": entry
+        })
+    # it tries in the rest of the entries for partial matches.
+    entries = util.list_entries()
+    matches = []
+    for entry in entries:
+        if entry.lower().find(title.lower()) >= 0:
+            matches.append(entry)
+    # if there are matches, we show the search page with a list of matches.
+    if matches:
+        return render(request, "encyclopedia/search.html", {
+            "title": title,
+            "matches": matches
+        })
+    # otherwise, we display an error page indicating there are no entries for the title.
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "error": f"There are no entries for '{title}'"
+        })
